6523b0edb42c7abbd8e8c1d1f829bdd8f84b634b
errotleugim <94770602+errotleugim@users.noreply.github.com>
2022-01-17 18:47:54 -0800

    Added first attempt at localstorage function, still not working properly

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,38 @@
 //Date display
 function fecha() {
-var fecha = moment().format("dddd, MMMM Do YYYY, h:mm:ss ");
+var fecha = moment().format("dddd, MMMM Do YYYY, HH:mm:ss ");
 var cdate = $("#currentDay");
 cdate.html(fecha);
 }
 setInterval(fecha, 1000);
 
+renderLastRegistered();
+var nineam = document.getElementById("#nueve");
+var nineamsave = document.querySelector("#ambutton");
 
+function renderLastRegistered() {
+    var nine = localStorage.getItem("9AM");
+    var nineam = document.getElementById("nueve");
+    
+    nineam.innerHTML = nine;
+  }
+
+
+nineamsave.addEventListener("click", function(event) {
+    event.preventDefault();
+    var nineam = document.getElementById("nueve").value;
+    console.log(nineam);
+    localStorage.setItem("9AM", nineam);
+})
+
+
+
+var m = moment();
+function kuji() {
+    var kyu = moment().startOf('day').add(9,'hours').format("hh:mm")
+   var m = moment();
+   console.log(m.isbefore.m);
+        
+
+}
+kuji();
